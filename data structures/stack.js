01da7641919ff4131340539eb8901591d44d3f6d
zessu <amackzie@gmail.com>
2018-07-29 20:27:36 +0300

    (feat): Create stacks js implementation - init project - create class for js stack implementation

---
+++ data structures/stack.js
@@ -0,0 +1,74 @@
+class Stack {
+
+  constructor() {
+    this.data = {};
+    this.count = 0;
+  }
+
+  /**
+   * adds an element to the top of the stack
+   * @param {any} element to add to stack
+   * @return { number } number of items on the stack
+   */
+  push(element) {
+    this.data[this.count] = element;
+    this.count++;
+    return this.count;
+  }
+
+  /**
+   * remove element from the top of the stack
+   * @return { number } number of elements on the stack
+   */
+  pop() {
+    if (this.count === 0) {
+      return;
+    }
+    this.count--;
+    delete this.data[this.count];
+    this.count--;
+  }
+
+  /**
+   * returns size of the stack
+   * @return {number} value of count
+   */
+  size() {
+    return this.count;
+  }
+
+  /**
+   * check whether the stack is emptry
+   * @return { boolean } whether the stack is empty or not
+   */
+  isEmpty() {
+    return this.count === 0;
+  }
+
+  /**
+   * return the element at the top of the stack
+   * @return {any} element at the top of the stack
+   */
+  peek() {
+    return this.data[count];
+  }
+
+  /**
+   * clear the stack
+   * @return { boolean } whether or not the stack was cleared
+   */
+  clear() {
+    this.data = {};
+    this.count = 0;
+  }
+
+  /**
+   * displays the items on the stack
+   */
+  print() {
+    return Object.values(this.data);
+  }
+
+}
+
+module.exports = Stack;
