0b1fbf48063372b5bdd41104918d51658ea423dd
zessu <amackzie@gmail.com>
2018-07-30 23:28:31 +0300

    (bubbleSort): add bubblesort alg and tests

---
+++ algorithms/bubbleSort.js
@@ -0,0 +1,14 @@
+export default function bubbleSort(array) {
+  for (let i = 0; i < array.length - 1; i += 1) {
+    let sorted = true;
+    for (let j = 0; j < (array.length - i - 1); j += 1) {
+      if (array[j] > array[j + 1]) {
+        const tmp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = tmp;
+        sorted = false;
+      }
+    }
+    if (sorted) break;
+  }
+}
