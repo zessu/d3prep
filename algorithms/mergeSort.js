5794fbf3cacf5b6dfef34f8d861f0b846aeafce2
zessu <amackzie@gmail.com>
2018-07-31 04:08:21 +0300

    (mergeSort): add merge sort algorithm and tests

--- algorithms/mergeSort.js
+++ algorithms/mergeSort.js
@@ -1,3 +1,22 @@
-export default mergeSort() {
-  
-} 
+function merge(left, right) {
+  const results = [];
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) {
+      results.push(left.shift());
+    } else {
+      results.push(right.shift());
+    }
+  }
+  return results.concat(left, right);
+}
+
+export default function mergeSort(array) {
+  if (array.length === 1) {
+    return array;
+  }
+  const middle = Math.floor(array.length / 2);
+  const left = array.slice(0, middle);
+  const right = array.slice(middle);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
